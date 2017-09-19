4f326e6cc037e05d45e6a76892df2f5836738065
Surender Singh Rathore <surenbika@gmail.com>
2017-09-19 17:00:35 +0530

    Initial commit.

---
+++ src/test/java/org/tektutor/AppTest.java
@@ -0,0 +1,26 @@
+package org.tektutor;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    /**
+     * @return the suite of tests being tested
+     */
+
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+       // assertTrue( true );
+    }
+}
